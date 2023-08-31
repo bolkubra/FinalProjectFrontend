8d0ae40a7cc6cdd7d5c4deb1528d3f8208eff300
Kübra Bol <kubra1272@icloud.com>
2023-08-31 23:58:01 +0300

    update

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Products } from 'src/app/models/product';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit{
 
 
   products:Products[] = []
+  //productResponseModel:ProductResponseModel={}
 
+  ngOnInit(): void {
+    console.log("init çalıştı");
+  }
 }
